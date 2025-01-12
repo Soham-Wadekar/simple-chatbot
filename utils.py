d05a756a89950149313d9602eb374d25230da48e
sohamw03@gmail.com <donbhai@yahoo.com>
2025-01-12 13:14:38 +0530

    utility function added

---
+++ utils.py
@@ -0,0 +1,11 @@
+from config import app, db
+from model import ChatHistory
+
+def display_db(app=app, db=db):
+
+    with app.app_context():
+        db.create_all()
+        print("Displaying chat messages:")
+        chats = ChatHistory.query.all()
+        for chat in chats:
+            print(f"{chat.sender}: {chat.message}")
